95d61efe5df0649d857d254f0a38f04326f0a684
Beyza Nur Karakoç <karak@LAPTOP-69B0RR13>
2024-01-02 21:01:34 +0300

    Standart kullanıcı

--- standartKullanici.Designer.cs
+++ standartKullanici.Designer.cs
@@ -28,12 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hesabaGitmebtn = new System.Windows.Forms.Button();
+            this.filmPagebtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cikisYapbtn = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(-3, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(804, 453);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.cikisYapbtn);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.filmPagebtn);
+            this.tabPage1.Controls.Add(this.hesabaGitmebtn);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(796, 420);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "AnaSayfa";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(796, 420);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Film";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PATSO FİLM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(684, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hesap Bilgileri";
+            // 
+            // hesabaGitmebtn
+            // 
+            this.hesabaGitmebtn.Location = new System.Drawing.Point(688, 27);
+            this.hesabaGitmebtn.Name = "hesabaGitmebtn";
+            this.hesabaGitmebtn.Size = new System.Drawing.Size(102, 26);
+            this.hesabaGitmebtn.TabIndex = 2;
+            this.hesabaGitmebtn.Text = "Hesabınız";
+            this.hesabaGitmebtn.UseVisualStyleBackColor = true;
+            // 
+            // filmPagebtn
+            // 
+            this.filmPagebtn.Location = new System.Drawing.Point(32, 293);
+            this.filmPagebtn.Name = "filmPagebtn";
+            this.filmPagebtn.Size = new System.Drawing.Size(167, 38);
+            this.filmPagebtn.TabIndex = 3;
+            this.filmPagebtn.Text = "Film";
+            this.filmPagebtn.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(280, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Patso Film ile film izlemeye hazır mıyız?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(701, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Çıkış Yap";
+            // 
+            // cikisYapbtn
+            // 
+            this.cikisYapbtn.Location = new System.Drawing.Point(688, 102);
+            this.cikisYapbtn.Name = "cikisYapbtn";
+            this.cikisYapbtn.Size = new System.Drawing.Size(99, 29);
+            this.cikisYapbtn.TabIndex = 6;
+            this.cikisYapbtn.Text = "Cıkış Yap";
+            this.cikisYapbtn.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(796, 420);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Kullanıcı";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // standartKullanici
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "standartKullanici";
             this.Text = "standartKullanici";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button cikisYapbtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button filmPagebtn;
+        private System.Windows.Forms.Button hesabaGitmebtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage3;
     }
 }
